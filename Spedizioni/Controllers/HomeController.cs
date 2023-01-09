77d24c323decb477e09556e4d9013128712cae03
danydifilippo <danydifilippo@gmail.com>
2023-01-09 18:35:26 +0100

    Aggiungere i file di progetto.

---
+++ Spedizioni/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Spedizioni.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
